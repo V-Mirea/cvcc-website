c9cf707a2cc965fc897f7814b8cfb045bb8c8917
SpeedOfSmell <vldmirea@gmail.com>
2015-11-15 15:18:12 -0500

    Fixed "scroll-queuing"

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
-$(document).ready(function() {
-  
+$(document).ready(function() { 
+    
+    var animating = false;
+    
     // If user moves the mousewheel
     $(window).bind('mousewheel', function(event) { 
         // Prevent the little scroll that would be caused by default
         event.preventDefault();
         
-        // If user scrolls down and is not at the bottom already
-        if ((event.deltaY < 0) && $(".current").next(".full-height").length > 0) {
-            $(".current").next(".full-height").addClass("current");
-            $(".current").first().removeClass("current");
-            
-            // Scroll to the top of the '.current' class in 1000ms
-            $("html, body").animate({
-                // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
-                scrollTop: $(".current").offset().top - 111   
-            }, 1000);
-            
-            $(".checked").next("li").addClass("checked");
-            $(".checked").first().removeClass("checked");
-        // If user scrolls up and is not at the top already
-        } else if ((event.deltaY > 0) && $(".current").prev(".full-height").length > 0) { 
-            if ($(".current").prev(".full-height").length > 0) { 
-                $(".current").prev(".full-height").addClass("current");
-                $(".current").last().removeClass("current");
-                
+        if (!animating) {
+            animating = true;
+            // If user scrolls down and is not at the bottom already
+            if ((event.deltaY < 0) && $(".current").next(".full-height").length > 0) {
+                $(".current").next(".full-height").addClass("current");
+                $(".current").first().removeClass("current");
+
+                // Scroll to the top of the '.current' class in 1000ms
                 $("html, body").animate({
-                    // CHANGE AFTER REPLACING PLACEHOLDER
-                    scrollTop: $(".current").offset().top - 111    
-                }, 1000);
-                
-                $(".checked").prev("li").addClass("checked");
-                $(".checked").last().removeClass("checked");
-            }            
+                    // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
+                    scrollTop: $(".current").offset().top - 111   
+                }, 1000, function() {animating = false;});
+
+                $(".checked").next("li").addClass("checked");
+                $(".checked").first().removeClass("checked");
+            // If user scrolls up and is not at the top already
+            } else if ((event.deltaY > 0) && $(".current").prev(".full-height").length > 0) { 
+                if ($(".current").prev(".full-height").length > 0) { 
+                    $(".current").prev(".full-height").addClass("current");
+                    $(".current").last().removeClass("current");
+
+                    $("html, body").animate({
+                        // CHANGE AFTER REPLACING PLACEHOLDER
+                        scrollTop: $(".current").offset().top - 111    
+                    }, 1000, function() {animating = false;});
+
+                    $(".checked").prev("li").addClass("checked");
+                    $(".checked").last().removeClass("checked");
+                }            
+            }
         }
     });
     
     // If user clicks on side-bar
     $(".glyphicon").click(function(event) {
         event.preventDefault();
-        // href of li element
-        var href = $(event.target).parent().attr('href');
         
-        $(".current").removeClass("current");
-        $(href).addClass("current");
-        
-        $(".checked").removeClass("checked");
-        $(event.target).parent().parent().addClass("checked");
-        
-        $("html, body").animate({
-                // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
-                scrollTop: $(".current").offset().top - 111   
-        }, 1000);
+        if (!animating) {
+            animating = true;
+            // href of li element
+            var href = $(event.target).parent().attr('href');
+
+            $(".current").removeClass("current");
+            $(href).addClass("current");
+
+            $(".checked").removeClass("checked");
+            $(event.target).parent().parent().addClass("checked");
+
+            $("html, body").animate({
+                    // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
+                    scrollTop: $(".current").offset().top - 111   
+            }, 1000, function() {animating = false;});
+        }
     });
 });
