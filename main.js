313c7f1f414d47ef44685fa0ddf68b7d4f443564
SpeedOfSmell <vldmirea@gmail.com>
2015-11-15 13:03:15 -0500

    Added vertical pagination

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ $(document).ready(function() {
                 // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
                 scrollTop: $(".current").offset().top - 111   
             }, 1000);
+            
+            $(".checked").next("li").addClass("checked");
+            $(".checked").first().removeClass("checked");
         // If user scrolls up and is not at the top already
         } else if ((event.deltaY > 0) && $(".current").prev(".full-height").length > 0) { 
             if ($(".current").prev(".full-height").length > 0) { 
@@ -25,7 +28,28 @@ $(document).ready(function() {
                     // CHANGE AFTER REPLACING PLACEHOLDER
                     scrollTop: $(".current").offset().top - 111    
                 }, 1000);
+                
+                $(".checked").prev("li").addClass("checked");
+                $(".checked").last().removeClass("checked");
             }            
         }
+    });
+    
+    // If user clicks on side-bar
+    $(".glyphicon").click(function(event) {
+        event.preventDefault();
+        // href of li element
+        var href = $(event.target).parent().attr('href');
+        
+        $(".current").removeClass("current");
+        $(href).addClass("current");
+        
+        $(".checked").removeClass("checked");
+        $(event.target).parent().parent().addClass("checked");
+        
+        $("html, body").animate({
+                // 111 px offset is due to navbar. CHANGE AFTER REPLACING PLACEHOLDER
+                scrollTop: $(".current").offset().top - 111   
+        }, 1000);
     });
 });
